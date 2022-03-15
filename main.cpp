e84bf068f2471e5fc7ccd9185a088913e39dc436
msaber2002 <95236892+msaber2002@users.noreply.github.com>
2022-03-15 16:16:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ int main(int argc, const char * argv[]) {
         }
     }*/
         
-        /*!!!!!!!!YOU HAVE TO MAKE A LOOP FOR BOTH VECTOS TO FIND SIMILAR LETTERS AND CRYPTE!!!!!!!*/
+        /*!!!!!!!!TWO METHODS ARE MENTIONNED IN THIS CODE, THE ONE COMMENTED IS LESS EFFICIENT THE OTHER ONE AND IT ENCIPER SIMILAR LETTERS BY SAME LETTERS!!!!!!!*/
         
     cout << "The text you inserted is : " << texttoo << endl;
     cout << "The encrypted version of the text is : " << endl;
